df3e6145c0631c6f51b6a71b8272c5db644a4ec1
JuanTLA16 <juan2234511@correo.uis.edu.co>
2023-04-22 01:04:30 -0500

    Se agregó el algoritmo

---
+++ Ejercicio_2/Ejercicio_2.py
@@ -0,0 +1,14 @@
+
+print("---------------------------------------")
+print("----------------Ejercicio 2------------")
+print("---------------------------------------")
+
+
+
+for i in range (1000, 5000):
+     rta = 5000/7 - 142
+     print("Los multiplos de 7 entre 1000  5000 son: " + str(rta))
+
+     rta2 = 5000/9 - 111
+     print("Los multiplos de 9 entre 1000  5000 son: " + str(rta2))
+     break
