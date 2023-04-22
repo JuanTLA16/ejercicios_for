17e8caf891a462feecbef1740178eb6d8f4619ac
JuanTLA16 <juan2234511@correo.uis.edu.co>
2023-04-21 23:40:53 -0500

    Se agregó el algoritmo

--- Ejercicio_1/Ejercicio_1.py
+++ Ejercicio_1/Ejercicio_1.py
@@ -1,11 +1,29 @@
-
 print("------------------------------------------")
 print("--------------EJERCICIO 1-----------------")
 print("------------------------------------------")
 
 N = int(input("Digite un numero entero: "))
 
-pares = 0
-impares = 0
+Pares = 0
+Impares = 0
 
-for i in 0 
+
+while (N != 0):
+
+    for i in range (1):
+
+        if ( N % 2 == 0):
+           print("El numero es PAR. ")
+           Pares = Pares + 1
+
+        else:
+            print("El numero es IMPAR. ")
+            Impares = Impares + 1
+
+
+        N = int(input("Digite un numero entero: "))
+
+
+print("La cantidad de numero PARES en total son: " + str(Pares))
+print("La cantidad de numeros IMPARES en total son: " + str(Impares))
+
